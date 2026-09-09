b7a0698302d971e4a18daf569310cd8009cba178
unknown <d:\1c\The Bat!>
2026-09-09 21:02:42 +0300

    fix: restore Cyrillic OCR recognition (add OCRConfig with rus language)
    - Add missing tesseract_ocr/ocr_engine_config import
    - Pass OCRConfig(language: 'rus', engine: OCREngine.tesseract) to extractText
    - Add assets/tessdata_config.json to pubspec assets

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:tesseract_ocr/tesseract_ocr.dart';
+import 'package:tesseract_ocr/ocr_engine_config.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:share_plus/share_plus.dart';
 
@@ -305,7 +306,17 @@ class _RestaurantSplitterScreenState extends State<RestaurantSplitterScreen> {
         });
       }
 
-      final String extractedText = await TesseractOcr.extractText(file.path);
+      final config = OCRConfig(
+        language: 'rus',
+        engine: OCREngine.tesseract,
+        options: {
+          'preserve_interword_spaces': '1',
+        },
+      );
+      final String extractedText = await TesseractOcr.extractText(
+        file.path,
+        config: config,
+      );
 
       final lines = extractedText.split(RegExp(r'[\n\r]+'));
       final foundItems = _extractItemsFromLines(lines);
